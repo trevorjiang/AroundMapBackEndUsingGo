21a78c77f6ad4b3ec8ea52f81c33aaf735e06318
Peixuan Jiang <peix8580@esri.com>
2018-02-05 20:09:31 -0800

    Change url

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.25.102:9200"
+	ES_URL = "http://35.229.32.185:9200"
 )
 
 func main() {
