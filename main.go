cbec8f898ff11793b754849488830fa5135ea27c
Peixuan Jiang <peix8580@esri.com>
2018-02-07 20:07:43 -0800

    Change to store bigtable

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"log"
 	"strconv"
 	"reflect"
+	//"github.com/pborman/uuid"
+	"cloud.google.com/go/bigtable"
+	"context"
 	"github.com/pborman/uuid"
 )
 
@@ -28,8 +31,8 @@ const (
 	TYPE = "post"
 	DISTANCE = "200km"
 	// Needs to update
-	//PROJECT_ID = "around-xxx"
-	//BT_INSTANCE = "around-post"
+	PROJECT_ID = "coastal-pointer-194222"
+	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://35.229.32.185:9200"
 )
@@ -85,6 +88,29 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	id := uuid.New()
 	// Save to ES.
 	saveToES(&p, id)
+
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+	mut.Set("post", "user", t, []byte("p.User"))
+	mut.Set("post", "message", t, []byte("p.Message"))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err1 := tbl.Apply(ctx, "com.google.cloud", mut)
+	if err1 != nil {
+		panic(err1)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 }
 
 // Save a post to ElasticSearch
